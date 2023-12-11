9312e9b369c997bd825cd682583174d0a8dbd9f0
Pastukhovalydia <pastuhovna@gmail.com>
2023-12-11 23:31:19 +0300

    TaskPlanner

---
+++ src/test/java/ru/netology/TodosTest.java
@@ -0,0 +1,89 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchByTitle() {
+        SimpleTask task1 = new SimpleTask(1, "Task 1");
+        SimpleTask task2 = new SimpleTask(2, "Task 2");
+        Todos todos = new Todos();
+        todos.add(task1);
+        todos.add(task2);
+
+        Task[] expected = {task2};
+        Task[] actual = todos.search("Task 2");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchBySubtaskInEpic() {
+        String[] subtasks = {"Subtask 1", "Subtask 2", "Subtask 3"};
+        Epic epic = new Epic(1, subtasks);
+        Todos todos = new Todos();
+        todos.add(epic);
+
+        Task[] expected = {epic};
+        Task[] actual = todos.search("Subtask 2");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchByTopicInMeeting() {
+        Meeting meeting = new Meeting(1, "Topic 1", "Project 1", "Start 1");
+        Todos todos = new Todos();
+        todos.add(meeting);
+
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("Topic 1");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindTask() {
+        SimpleTask task = new SimpleTask(1, "Task");
+        Todos todos = new Todos();
+        todos.add(task);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Non-existing task");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindTaskInEmptyList() {
+        Todos todos = new Todos();
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Task");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
